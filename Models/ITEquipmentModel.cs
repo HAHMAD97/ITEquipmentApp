e27098261cc03d9f6b0f523013d563d761bdca33
Hira <ahmadhira8@gmail.com>
2025-10-07 20:56:30 -0400

    added data annotations

--- Models/ITEquipmentModel.cs
+++ Models/ITEquipmentModel.cs
@@ -1,17 +1,19 @@
+using System.ComponentModel.DataAnnotations;
 namespace ITEquipmentBorrowApp.Models;
+
 public class ITEquipment
 {
     public int Id { get; set; }
-    
+
     [Required]
     public EquipmentType Type { get; set; }
-   
+
     [Required, StringLength(200, MinimumLength = 10)]
-    public string Description { get; set; } 
-    
+    public string Description { get; set; }
+
     [Required]
     public bool IsAvailable { get; set; }
-    
+
 }
 
 public enum EquipmentType
